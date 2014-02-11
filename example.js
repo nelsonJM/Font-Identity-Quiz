92aa88125d436017fe5cdc9dd1602e7652476d56
Josh Nelson <jnelson@steelsentry.com>
2014-02-11 12:38:12 -0600

    initial commit

---
+++ example.js
@@ -0,0 +1,13 @@
+// EventUtil.addHandler(window, "load", function(event){
+// 	var div = document.createElement("div");
+// 	div.id = "notice";
+// 	document.body.insertBefore(div, document.body.firstChild);
+
+// var closeNotice = document.querySelector(".close-notice");
+// var notice = document.getElementById("notice");
+// 	EventUtil.addHandler(closeNotice, "click", function(event){
+// 		event = EventUtil.getEvent(event);
+// 		EventUtil.preventDefault(event);
+// 		notice.setAttribute("class", "hide");
+// 	});
+// });

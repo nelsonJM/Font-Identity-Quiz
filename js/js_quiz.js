463c95fe207430159f9dccf6618bb0d63ebd1a1b
Josh Nelson <jmarknel@gmail.com>
2014-06-19 08:19:14 -0500

    rename variables

--- js/js_quiz.js
+++ js/js_quiz.js
@@ -1,53 +1,54 @@
 EventUtil.addHandler(window, "load", function() {
 
-	var question1 = {
-		question: "What's Josh's favorite color?",
-		choices: ["red", "green", "blue"],
-		correctAnswer: 1
-	},
+	
 
-	question2 = {
-		question: "What's Josh's favorite animal?",
-		choices: ["dog", "rabbit", "cat"],
-		correctAnswer: 2
-	},
 
-	question3 = {
-		question: "What's Josh's favorite food?",
-		choices: ["enchiladas", "green chile", "blue cheese dressing"],
-		correctAnswer: 0
-	},
-
-	allQuestions = [question1, question2, question3],
-
-	qLength = allQuestions.length,
-
+	var
+	// allQuestions = [question1, question2, question3],
+	qLength = 0;
 	i=0,
 	c=0,
 	score=0,
-
 	yourChoices = [],
 
 	nextBtn = document.getElementById("qbtn"),
 	backBtn = document.getElementById("bbtn"),
 
+	grabQuestions = function() {
+		$.getJSON('js/questions.json', function(data) {
+			console.log(data);
+			getStarted(data);
+			
+		});
+	},
+
+	getStarted = function(jsonData) {
+		window.data = jsonData;
+		qLength = data.questions.length;
+		loadQuestion();
+		loadChoiceInputs();
+		loadChoices();
+	},
+
+
 	loadQuestion = function() {
-		document.getElementById("question").innerHTML = "<p data-question="+allQuestions.indexOf(allQuestions[i])+">" + allQuestions[i].question + "</p>";
+		document.getElementById("question").innerHTML = "<p data-question="+data.questions.indexOf(data.questions[i])+">" + data.questions[i].question + "</p>";
 		
 	},
 
 	loadChoiceInputs = function() {
-		var qchoices = allQuestions[i].choices;
+		var qchoices = data.questions[i].choices;
+		// var qchoices = allQuestions[i].choices;
 		var len = qchoices.length;
 
 		for (var c=0; c<len; c++) {
 			var qChoice = document.createElement("input");
 				qChoice.type = "radio";
 				qChoice.name = "pick";
-				qChoice.value = allQuestions[i].choices[c];
+				qChoice.value = qchoices[c];
 			var qChoiceLabel = document.createElement("label");
 
-			if (allQuestions[i].choices[c] === yourChoices[i]) {
+			if (yourChoices[i] && qchoices[c] === yourChoices[i]) {
 				qChoice.checked = true;
 
 			} else {
@@ -75,7 +76,7 @@ EventUtil.addHandler(window, "load", function() {
 
 		for (var c=0; c<len; c++) {
 			// currentChoices[c].value = allQuestions[i].choices[c];
-			cLabels[c].innerHTML = allQuestions[i].choices[c];
+			cLabels[c].innerHTML = data.questions[i].choices[c];
 		}
 	},
 
@@ -85,7 +86,7 @@ EventUtil.addHandler(window, "load", function() {
 		document.getElementById("question").innerHTML = "Thanks for playing. Your Score is: ";
 		document.getElementById("answers").innerHTML = score;
 		document.getElementById("controls").removeChild(nextBtn);
-	};
+	},
 
 	fadeOut = function() {
 		var question = document.getElementById("question");
@@ -95,7 +96,7 @@ EventUtil.addHandler(window, "load", function() {
 		$(question).find('p').addClass("fadeOut");
 		$(currentChoices).addClass("fadeOut");
 		$(cLabels).addClass("fadeOut");
-	};
+	},
 
 	allTogether = function() {
 		loadQuestion();
@@ -120,12 +121,12 @@ EventUtil.addHandler(window, "load", function() {
 		var currentChoices = document.querySelectorAll('#answers input');
 		var len = currentChoices.length;
 		var notAnswered = 1;
-		var theAnswer = allQuestions[i].correctAnswer;
+		var theAnswer = data.questions[i].correctAnswer;
 		
 		for (var c=0; c<len; c++){
 				if (currentChoices[c].checked) {
 
-					if ( c === allQuestions[i].correctAnswer && !yourChoices[i] && allQuestions[i] != allQuestions[qLength - 1]) {
+					if ( c === theAnswer && !yourChoices[i] && data.questions[i] != data.questions[qLength - 1]) {
 
 						yourChoices[i] = currentChoices[c].value;
 						console.log(yourChoices[i]);
@@ -141,7 +142,7 @@ EventUtil.addHandler(window, "load", function() {
 
 						setTimeout(allTogether, 500);
 					
-					} else if ( c === allQuestions[i].correctAnswer && yourChoices[i] && yourChoices[i] === currentChoices[c].value && allQuestions[i] != allQuestions[qLength - 1]) {
+					} else if ( c === theAnswer && yourChoices[i] && yourChoices[i] === currentChoices[c].value && data.questions[i] != data.questions[qLength - 1]) {
 
 						yourChoices[i] = currentChoices[c].value;
 						
@@ -156,7 +157,7 @@ EventUtil.addHandler(window, "load", function() {
 
 						setTimeout(allTogether, 500);
 
-					} else if ( c === allQuestions[i].correctAnswer && yourChoices[i] && yourChoices[i] != allQuestions[i].choices[theAnswer] && allQuestions[i] != allQuestions[qLength - 1]) {
+					} else if ( c === theAnswer && yourChoices[i] && yourChoices[i] != data.questions[i].choices[theAnswer] && data.questions[i] != data.questions[qLength - 1]) {
 
 						yourChoices[i] = currentChoices[c].value;
 						
@@ -172,7 +173,7 @@ EventUtil.addHandler(window, "load", function() {
 
 						setTimeout(allTogether, 500);
 
-					} else if (c === allQuestions[i].correctAnswer && allQuestions[i] === allQuestions[qLength - 1]) {
+					} else if (c === theAnswer && data.questions[i] === data.questions[qLength - 1]) {
 
 						yourChoices[i] = currentChoices[c].value;
 
@@ -184,7 +185,7 @@ EventUtil.addHandler(window, "load", function() {
 						setTimeout(removeInputs, 500);
 						setTimeout(finalPage, 500);
 
-					} else if (c != allQuestions[i].correctAnswer && !yourChoices[i] && allQuestions[i] === allQuestions[qLength - 1]) {
+					} else if (c != theAnswer && !yourChoices[i] && data.questions[i] === data.questions[qLength - 1]) {
 
 						yourChoices[i] = currentChoices[c].value;
 						
@@ -196,9 +197,8 @@ EventUtil.addHandler(window, "load", function() {
 						setTimeout(finalPage, 500);
 
 						
-					} else if (c != allQuestions[i].correctAnswer && yourChoices[i] && yourChoices[i] != allQuestions[i].choices[theAnswer] && allQuestions[i] != allQuestions[qLength - 1]) {
-						console.log(i);
-						console.log(allQuestions[i].choices[theAnswer]);
+					} else if (c != theAnswer && yourChoices[i] && yourChoices[i] != data.questions[i].choices[theAnswer] && data.questions[i] != data.questions[qLength - 1]) {
+						
 						yourChoices[i] = currentChoices[c].value;
 						
 						alert("hmm");
@@ -214,7 +214,7 @@ EventUtil.addHandler(window, "load", function() {
 
 						setTimeout(allTogether, 500);
 
-					} else if (c != allQuestions[i].correctAnswer && yourChoices[i] && yourChoices[i] === allQuestions[i].choices[theAnswer] && allQuestions[i] != allQuestions[qLength - 1]) {
+					} else if (c != theAnswer && yourChoices[i] && yourChoices[i] === data.questions[i].choices[theAnswer] && data.questions[i] != data.questions[qLength - 1]) {
 
 						yourChoices[i] = currentChoices[c].value;
 						
@@ -231,7 +231,7 @@ EventUtil.addHandler(window, "load", function() {
 
 						setTimeout(allTogether, 500);
 
-					} else if (c != allQuestions[i].correctAnswer && !yourChoices[i] && allQuestions[i] != allQuestions[qLength - 1]) {
+					} else if (c != theAnswer && !yourChoices[i] && data.questions[i] != data.questions[qLength - 1]) {
 
 						yourChoices[i] = currentChoices[c].value;
 						
@@ -261,8 +261,8 @@ EventUtil.addHandler(window, "load", function() {
 		
 	});
 
-	loadQuestion();
-	loadChoiceInputs();
-	loadChoices();
+	
+grabQuestions();
+	
 
 });

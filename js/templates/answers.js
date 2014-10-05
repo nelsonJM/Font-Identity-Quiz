8178e093de2e489ad42ba58ae5d0afc1e69150f7
Josh Nelson <jmarknel@gmail.com>
2014-10-05 08:32:55 -0500

    make row and col for each answer

--- js/templates/answers.js
+++ js/templates/answers.js
@@ -1,32 +1,32 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['answers'] = template({"1":function(depth0,helpers,partials,data) {
-  var stack1, helper, lambda=this.lambda, escapeExpression=this.escapeExpression, functionType="function", helperMissing=helpers.helperMissing, buffer = "		<div class=\"review\">\n			<div class=\"qa\">\n				<div class=\"answer-name\">\n					<span>Font name:</span> "
+  var stack1, helper, lambda=this.lambda, escapeExpression=this.escapeExpression, functionType="function", helperMissing=helpers.helperMissing, buffer = "		<div class=\"review\">\n			\n					<div class=\"qa\">\n						<div class=\"row\">\n							<div class=\"col-1\">\n								<div class=\"answer-name\">\n									<span>Font name:</span> "
     + escapeExpression(lambda((depth0 != null ? depth0.correctAnswerName : depth0), depth0))
-    + "\n				</div>\n				<div class=\"question\">\n						\n					<div data-question=\""
+    + "\n								</div>\n							</div>\n						</div>\n						<div class=\"row\">\n							<div class=\"col-1\">\n								<div class=\"question\">\n										\n									<div data-question=\""
     + escapeExpression(lambda((depth0 != null ? depth0.questionIndex : depth0), depth0))
-    + "\">\n						<img src='"
+    + "\">\n										<img src='"
     + escapeExpression(((helper = (helper = helpers.src || (depth0 != null ? depth0.src : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"src","hash":{},"data":data}) : helper)))
-    + "'/>\n					</div>\n					\n				</div>\n				<div class=\"response-answer\">\n					<div class=\"answers\">\n						<div class=\"info-text\">\n							<span>You answered...</span>\n						</div>\n";
+    + "'/>\n									</div>\n									\n								</div>\n							</div>\n						</div>\n						<div class=\"row\">\n							<div class=\"col-1\">\n								<div class=\"response-answer\">\n									<div class=\"answers\">\n										<div class=\"info-text\">\n											<span>You answered...</span>\n										</div>\n";
   stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.options : depth0), {"name":"each","hash":{},"fn":this.program(2, data),"inverse":this.noop,"data":data});
   if (stack1 != null) { buffer += stack1; }
-  return buffer + "\n					</div>\n					\n				</div>\n				\n				\n			</div>\n			\n		</div>\n";
+  return buffer + "\n									</div>\n									\n								</div>\n							</div>\n						</div>\n						\n					</div>\n		</div>\n";
 },"2":function(depth0,helpers,partials,data) {
-  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, buffer = "						<div class=\"answer-choices\" data-answer=\""
+  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, buffer = "										<div class=\"answer-choices\" data-answer=\""
     + escapeExpression(((helper = (helper = helpers.isCorrect || (depth0 != null ? depth0.isCorrect : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"isCorrect","hash":{},"data":data}) : helper)))
     + "\" data-choice=\""
     + escapeExpression(((helper = (helper = helpers.checked || (depth0 != null ? depth0.checked : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"checked","hash":{},"data":data}) : helper)))
     + "\">\n";
   stack1 = helpers['if'].call(depth0, (depth0 != null ? depth0.checked : depth0), {"name":"if","hash":{},"fn":this.program(3, data),"inverse":this.program(5, data),"data":data});
   if (stack1 != null) { buffer += stack1; }
-  return buffer + "							<label for=\""
+  return buffer + "											<label for=\""
     + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
     + "\">"
     + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
-    + "</label>\n						</div>\n";
+    + "</label>\n										</div>\n";
 },"3":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, lambda=this.lambda;
-  return "								<input type=\"radio\" id=\""
+  return "												<input type=\"radio\" id=\""
     + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
     + "\" class=\""
     + escapeExpression(((helper = (helper = helpers.checked || (depth0 != null ? depth0.checked : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"checked","hash":{},"data":data}) : helper)))
@@ -37,7 +37,7 @@ templates['answers'] = template({"1":function(depth0,helpers,partials,data) {
     + "\" checked=\"checked\" />\n";
 },"5":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, lambda=this.lambda;
-  return "								<input type=\"radio\" id=\""
+  return "												<input type=\"radio\" id=\""
     + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
     + "\" class=\""
     + escapeExpression(((helper = (helper = helpers.checked || (depth0 != null ? depth0.checked : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"checked","hash":{},"data":data}) : helper)))
--- js/templates/qcontent.js
+++ js/templates/qcontent.js
@@ -14,13 +14,13 @@ templates['qcontent'] = template({"1":function(depth0,helpers,partials,data) {
     + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
     + "</label>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, buffer = "<div id=\"question\">\n				\n	<div data-question=\""
+  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, buffer = "<div class=\"row\">\n<div class=\"col-1\">\n<div id=\"question\">\n				\n	<div data-question=\""
     + escapeExpression(((helper = (helper = helpers.questionIndex || (depth0 != null ? depth0.questionIndex : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"questionIndex","hash":{},"data":data}) : helper)))
     + "\">\n		<img src='"
     + escapeExpression(((helper = (helper = helpers.src || (depth0 != null ? depth0.src : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"src","hash":{},"data":data}) : helper)))
     + "'/>\n	</div>\n\n</div>\n<div id=\"answers\">\n";
   stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.options : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
   if (stack1 != null) { buffer += stack1; }
-  return buffer + "</div>";
+  return buffer + "</div>\n</div>\n</div>";
 },"useData":true});
 })();

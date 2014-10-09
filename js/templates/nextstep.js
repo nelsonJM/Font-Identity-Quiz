da7c752b83a35315924a0d3e2cb0103f76b599c9
Josh Nelson <jmarknel@gmail.com>
2014-10-08 22:17:43 -0500

    bold links

--- js/templates/nextstep.js
+++ js/templates/nextstep.js
@@ -4,6 +4,6 @@ templates['nextstep'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":functi
   var lambda=this.lambda, escapeExpression=this.escapeExpression;
   return "<div class=\"nextSteps\">\n	<p>\n		Review your answers below, <a href=\"/"
     + escapeExpression(lambda((depth0 != null ? depth0.quizType : depth0), depth0))
-    + "-quiz/\">replay</a>, or <a href=\"/\">try another quiz</a>.\n	</p>\n</div>	";
+    + "-quiz/\" class=\"dotted\">replay</a>, or <a href=\"/\" class=\"dotted\">try another quiz</a>.\n	</p>\n</div>	";
 },"useData":true});
 })();
